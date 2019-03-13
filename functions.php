98c98e0172fadef69eec0b63b51d967667002269
Hans Kellerhuis <webtak@gmail.com>
2019-03-13 15:20:20 +0100

    Dont;s show show starter img  in admin

--- functions.php
+++ functions.php
@@ -3,4 +3,6 @@
  * Nothing more then a require_once document for all functions.
  */
 
-echo '<center><img width="50%" src="'.get_bloginfo('stylesheet_directory').'/screenshot.png"></center>';
+if(! is_admin() ){
+    echo '<center><img width="50%" src="'.get_bloginfo('stylesheet_directory').'/screenshot.png"></center>';
+}
