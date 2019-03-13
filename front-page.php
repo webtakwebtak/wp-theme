15b080a4101cfe21cfc7eeef1e8c65365793991e
Hans Kellerhuis <webtak@gmail.com>
2019-03-13 15:06:28 +0100

    Changed theme show screenshot on install

---
+++ front-page.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<div class="container">
+    <div class="row">
+
+        <img width="100%" src="<?php echo bloginfo('stylesheet_url'); ?>screenshot.png" alt="Screenshot thema">
+
+    </div>
+</div>
+
+<?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -2,3 +2,5 @@
 /**
  * Nothing more then a require_once document for all functions.
  */
+
+echo '<center><img width="50%" src="'.get_bloginfo('stylesheet_directory').'/screenshot.png"></center>';
