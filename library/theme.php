fef0450c59c368cf71736ed934ea33ddc3298fd5
Hans Kellerhuis <webtak@gmail.com>
2019-03-17 22:37:09 +0100

    Changes theme

--- library/theme.php
+++ library/theme.php
@@ -16,11 +16,29 @@ class Theme
         //add bundje for js/css
         add_action( 'wp_enqueue_scripts', array( $this, 'theme_enqueue_scripts' ));
         
+        // Remove default image sizes here.
+        add_filter( 'intermediate_image_sizes_advanced', array( $this, 'prefix_remove_default_images' ));
+        
+        // Remove default image sizes here.
+        add_filter( 'wp_handle_upload', array( $this, 'wpse_256351_upload'), 10, 2  );
+        
         //ajax
         add_action( 'wp_ajax_my_action', array( $this, 'my_action' ));
         add_action( 'wp_ajax_nopriv_my_action', array( $this, 'my_action' ));
     }
 
+    function wpse_256351_upload( $upload, $context ) {
+        
+    }
+    
+    function prefix_remove_default_images( $sizes ) {
+        unset( $sizes['small']); // 150px
+        unset( $sizes['medium']); // 300px
+        unset( $sizes['large']); // 1024px
+        unset( $sizes['medium_large']); // 768px
+        return $sizes;
+    }
+    
     public function theme_enqueue_scripts() {
         wp_enqueue_script( 'bundle', get_stylesheet_directory_uri() . '/assets/dist/js/bundle.js','', 1, false );
         wp_localize_script( 'bundle', 'ajax_object',array( 'ajax_url' => admin_url( 'admin-ajax.php' ), 'we_value' => 1234 ) ); 
