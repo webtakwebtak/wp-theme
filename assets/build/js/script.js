d957aec2ebff3f1a9b1bb63ea913ffa87d5c0ac1
Hans Kellerhuis <webtak@gmail.com>
2019-03-19 13:38:21 +0100

    Created a plugin from repsonsive images js

--- assets/build/js/script.js
+++ assets/build/js/script.js
@@ -1,6 +1,8 @@
 import $ from 'jquery';
-import jQuery from 'jquery'
+window.jQuery = $;
+window.$ = $;
 import 'slick-carousel';
+
 require('./wp-responsive-images.js');
 
 $( document ).ready(function() {
@@ -20,114 +22,34 @@ $( document ).ready(function() {
     	  infinite: false,
     	  speed: 300,
     });
-    
-    
-    $.fn.isInViewport = function() {
-    	var elementTop = $(this).offset().top;
-    	var elementBottom = elementTop + $(this).outerHeight();
-    	var viewportTop = $(window).scrollTop();
-    	var viewportBottom = viewportTop + $(window).height();
-    	if( elementBottom > viewportTop && elementTop < viewportBottom ){
 
-    		//get image
-    		var url 		= $(this).attr('src');
-    		var lastslash	= url.lastIndexOf('/');
-    		var fullname 	= url.substring(lastslash+1);
-    		var lasthyphen 	= fullname.lastIndexOf('-');
-    		var shortname 	= fullname.substring(0,lasthyphen);
-    		
-    		console.log("shortname:"+shortname);
-    		
-    		//slick
-    		//if($(this).parent().hasClass('slide') && !$(this).closest(".slick-slide").hasClass('slick-active')){
-    		//	return false;
-    		//}
-    		
-    		//if low res img
-			if( fullname.includes("x") ){
-	
-	    			//get agttributes img
-					var path 		= url.substring(0,lastslash)+'/';
-		    		var lasthyphen 	= fullname.lastIndexOf('-');
-		    		var lastdot 	= fullname.lastIndexOf('.');
-		    		var size 		= fullname.substring(lasthyphen+1,lastdot);
-		    		var ext 		= fullname.substring(lastdot+1);
-		    		var sizes 		= size.split("x");
-		    		var imgwidth	= $(this).width();
-		    		var imgratio	= sizes[0]/sizes[1];
-		    		var oriwidth	= $(this).data('media-width');
-		    		var oriheight	= $(this).data('media-height');
-		    		var imgratio	= oriwidth/oriheight;
-		    		var breakpoint  = null;
-		    		var breakpoints = [];
-		    		breakpoints[0] 	= 1140;
-		    		breakpoints[1] 	= 960;
-		    		breakpoints[2] 	= 720;
-		    		breakpoints[3] 	= 540;
-		    		breakpoints[4] 	= 300;
-		    		
-		    		if (imgwidth >= breakpoints[0] ) { breakpoint = null; } else
-		    			if (imgwidth >= breakpoints[1] && oriwidth >= breakpoints[0] ) { breakpoint = 0 } else
-		    				if (imgwidth >= breakpoints[2] && oriwidth >= breakpoints[1] ) { breakpoint = 1; } else
-		    					if (imgwidth >= breakpoints[3] && oriwidth >= breakpoints[2] ) { breakpoint = 2; } else
-		    						if (imgwidth >= breakpoints[4] && oriwidth >= breakpoints[3] ) { breakpoint = 3; } 
-		    
-		    		if( breakpoint == null){
-		    			var imgname		= shortname+'.'+ext;
-		    		}
-		    		else{
-		    			var newheight	= Math.round(breakpoints[breakpoint]/imgratio);
-			    		var imgname		= shortname+'-'+breakpoints[breakpoint]+'x'+newheight+'.'+ext;
-			    		$(this).attr("src", path+imgname);
-			    		$(this).addClass("loaded");
-		    		}
-		    		
-		    		/*
-		    		console.log("url:"+url);
-		    		console.log("fullname:"+fullname);
-		    		console.log("shortname:"+shortname);
-		    		console.log("size:"+size);
-		    		console.log("width:"+sizes[0]);
-		    		console.log("height:"+sizes[1]);
-		    		console.log("ext:"+ext);
-		    		console.log("display-width:"+imgwidth);
-		    		console.log("imgratio:"+imgratio);
-		    		console.log("newheight:"+newheight);
-		    		console.log("imgname:"+imgname);
-		    		*/
-			}
-			
-    	}
-    };
-    
-
-    //scroll
-	$(window).on("scroll", function() {
-		 $("img").not('.loaded,.notfirst').each(function() {  
-			$(this).isInViewport();
-	     }); 
-	});	
-	
-	var windowwidth = $(window).width();
-	 //only if window enlarged every 100px step
-	$(window).on("resize", function() {
-		if( $(window).width() > windowwidth + 100 ){
-		 $("img").not('.notfirst').each(function() {  
-			$(this).isInViewport();
-	     }); 
-		 windowwidth = $(window).width();
-		 $("img").removeClass("loaded");
-		}
-	});	
-	
-    //load
-	 $("img").not('.loaded,.notfirst').each(function() {  
-		 $(this).isInViewport();
-     }); 
-	 
-	// On before slide change
-	 $(".slider-wrapper").on('afterChange', function(event, slick, currentSlide){
-		 $(this).find('.slick-current img').isInViewport();
-	 });
-	
+    //call responsive-image-plugin
+    $(window).replaceImagesViewport({
+    	debug: true,
+		classloaded: 'loaded', 
+		classexclude: 'excluded', 
+		responsive: [
+			 {
+				 breakpoint: 1140,
+				 imagesize: 'full'
+			 },
+			 {
+				 breakpoint: 960,
+				 imagesize: 'full'
+			 },
+			 {
+				 breakpoint: 720,
+				 imagesize: 960
+			 },
+			 {
+				 breakpoint: 540,
+				 imagesize: 720
+			 },
+			 {
+				 breakpoint: 400,
+				 imagesize: 540
+			 }
+	     ]	
+    });
+      
 });
---
+++ assets/build/js/wp-responsive-images.js
@@ -0,0 +1,190 @@
+(function($) {
+    $.fn.replaceImages = function(options) {
+    	var imgfeatures = {
+    			url: '',
+    			fullname: '',
+    			shortname: '',
+    			path: '',
+    			ext: '',
+    			displaywidth: '',
+    			originalwidth: '',
+    			ratio: ''
+    	}
+    	
+    	var settings = $.extend({
+    		debug: false,
+    		classloaded: 'loaded', 
+    		classexclude: 'excluded', 
+    		responsive: [
+				 {
+					 breakpoint: 1140,
+					 imagesize: 'full'
+				 },
+				 {
+					 breakpoint: 960,
+					 imagesize: 1140
+				 },
+				 {
+					 breakpoint: 720,
+					 imagesize: 960
+				 },
+				 {
+					 breakpoint: 540,
+					 imagesize: 720
+				 },
+				 {
+					 breakpoint: 300,
+					 imagesize: 540
+				 }
+		     ]	
+        }, options );
+    	
+        this.each( function() {
+        	//only active slide images
+        	var slide = $(this).closest('.slick-slide');
+        	if(slide.length > 0 && !slide.hasClass('slick-current')){
+        		return false;
+        	}
+        	
+        	//only images in viewport
+        	if(isImageInViewport(this)){
+	        	//exlude loaded en excluded selectors
+	        	if( !$(this).hasClass(settings.classloaded,settings.classexclude) ){
+	        		//only image tha are not full sized already	
+		        	if(isImageNotFullVersion(this)){
+		        		loadBestOptionImage(this);
+		        	}
+	        	}
+        	}
+        	return false;
+        });
+        
+        function isImageInViewport(obj) {
+        	var elementTop = $(obj).offset().top;
+        	var elementBottom = elementTop + $(obj).outerHeight();
+        	var viewportTop = $(window).scrollTop();
+        	var viewportBottom = viewportTop + $(window).height();
+        	return ( elementBottom > viewportTop && elementTop < viewportBottom );
+        };
+        
+        function isImageNotFullVersion(obj) {
+        	imgfeatures.url 		= $(obj).attr('src');
+        	imgfeatures.fullname 	= imgfeatures.url.substring(imgfeatures.url.lastIndexOf('/')+1);
+    		if( imgfeatures.fullname.includes("x") ){
+    			return true;
+    		}
+    		return false;
+        };
+        
+        function loadBestOptionImage(obj) {
+        	var index;
+        	var imgname;
+        	var newheight;
+        	var imagesize;
+        	var breakpoint;
+        	getAllImageAttributes(obj);
+        	if(index = getNewImageIndex()){
+        		imagesize 	= settings.responsive[index].imagesize;
+        		breakpoint 	= settings.responsive[index].breakpoint;
+        		if( imagesize == 'full'){
+        			imgname	= imgfeatures.shortname+'.'+imgfeatures.ext;
+        		}
+        		else{
+        			newheight	= Math.round(imagesize/imgfeatures.ratio);
+    	    		imgname		= imgfeatures.shortname+'-'+imagesize+'x'+newheight+'.'+imgfeatures.ext;
+        		}
+        		$(obj).attr("src", imgfeatures.path+imgname);
+	    		$(obj).addClass("loaded");
+        	}
+        };
+        
+        function getNewImageIndex() {
+        	for (index = 0; index <= settings.responsive.length - 1; ++index) {
+        		var imagesize 	= settings.responsive[index].imagesize;
+        		var breakpoint 	= settings.responsive[index].breakpoint;
+        		if(imagesize == 'full'){
+        			imagesize = imgfeatures.originalwidth;
+        		}
+        		if( imgfeatures.displaywidth >= breakpoint && imgfeatures.originalwidth >= imagesize  ){
+        			return index;
+        		}
+        	}
+        	//return last option
+        	return (index-1);
+        };
+        
+        function getAllImageAttributes(obj) {
+        	var lasthyphen 				= imgfeatures.fullname.lastIndexOf('-');
+        	var lastslash 				= imgfeatures.url.lastIndexOf('/');
+    		var lastdot 				= imgfeatures.fullname.lastIndexOf('.');
+    		var originalwidth			= $(obj).data('media-width');
+    		var originalheight			= $(obj).data('media-height');
+    		
+    		imgfeatures.shortname 		= imgfeatures.fullname.substring(0,lasthyphen);
+    		imgfeatures.path 			= imgfeatures.url.substring(0,lastslash)+'/';
+    		imgfeatures.ext 			= imgfeatures.fullname.substring(lastdot+1);
+    		imgfeatures.displaywidth	= $(obj).width();
+    		imgfeatures.originalwidth	= originalwidth;
+    		imgfeatures.ratio			= originalwidth/originalheight;
+    		debug();
+        };
+        
+        function debug() {
+        	if(settings.debug){
+	        	console.log("url:"+imgfeatures.url);
+	    		console.log("fullname:"+imgfeatures.fullname);
+	    		console.log("shortname:"+imgfeatures.shortname);
+	    		console.log("path:"+imgfeatures.path);
+	    		console.log("ext:"+imgfeatures.ext);
+	    		console.log("display-wdth:"+imgfeatures.displaywidth);
+	    		console.log("imgratio:"+imgfeatures.ratio);
+	    		console.log("originalwidth:"+imgfeatures.originalwidth);
+        	}
+        };
+        
+    }
+})(jQuery);
+
+(function($) {
+    $.fn.replaceImagesViewport = function(options) {
+    	
+    	var windowwidth = $(this).width();
+    	var resizesteps = 150;
+    	var scrolltop 	= $(this).scrollTop();
+    	var scrollsteps = 150;
+
+    	//on load
+    	$(this).ready(function () {
+    		$("img").each(function() {  
+   				$(this).replaceImages(options);
+   		    }); 
+    	});
+    	
+    	//on scroll in steps
+    	$(this).on("scroll", function() {
+    		if( Math.abs($(this).scrollTop() - scrolltop) > scrollsteps ){
+	    		$("img").each(function() {  
+	    			 $(this).replaceImages(options);
+	    	    }); 
+	    		scrolltop = $(this).scrollTop();
+    		}
+    	});	
+    	
+    	//on resize in steps 
+    	$(this).on("resize", function() {
+	   		if( $(this).width() > windowwidth + resizesteps ){
+	   			$("img").removeClass("loaded");
+	   			$("img").each(function() {  
+	   				$(this).replaceImages(options);
+	   		    }); 
+	   			windowwidth = $(this).width();
+	   		}
+   	 	});	
+    	
+    	//on load slide with slick slider
+    	 $(".slider-wrapper").on('afterChange', function(event, slick, currentSlide){
+			$(this).find(".slick-current img").replaceImages(options);
+		 });
+
+    }
+})(jQuery);
